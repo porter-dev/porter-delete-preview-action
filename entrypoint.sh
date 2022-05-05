66dd9c0a5bcd06229b5ce369b142c790bbc4196a
Mohammed Nafees <hello@mnafees.me>
2022-05-05 19:17:03 +0530

    update for deployment_id

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ export PORTER_HOST=${INPUT_HOST}
 export PORTER_CLUSTER=${INPUT_CLUSTER:?input \"cluster\" not set or empty}
 export PORTER_PROJECT=${INPUT_PROJECT:?input \"project\" not set or empty}
 export PORTER_TOKEN=${INPUT_TOKEN:?input \"token\" not set or empty}
-export PORTER_ENVIRONMENT_ID=${INPUT_ENVIRONMENT_ID:?input \"environment_id\" not set or empty}
-export PORTER_PR_NUMBER=${INPUT_PR_NUMBER:?input \"pr_number\" not set or empty}
-export PORTER_REPO_NAME=${INPUT_REPO_NAME:?input \"repo_name\" not set or empty}
-export PORTER_REPO_OWNER=${INPUT_REPO_OWNER:?input \"repo_owner\" not set or empty}
+export PORTER_DEPLOYMENT_ID=${INPUT_DEPLOYMENT_ID:?input \"deployment_id\" not set or empty}
 
 porter delete
